1aa3ecd88c21c8d7ecf474d87e2f37ca09d0c9d6
yuki <yuqi01.chen@akesobio.com>
2024-05-31 16:20:52 +0800

    fix: fix issue of skipping blank pages

--- src/probe.rs
+++ src/probe.rs
@@ -12,12 +12,15 @@ pub fn probe(pdf_path: &Path) -> anyhow::Result<Report> {
             r.append_void(page_number + 1);
             continue;
         }
-        for row in page {
+        for (row_number, row) in page.iter().enumerate() {
             if row.is_empty() {
                 continue;
             }
             let row = row.trim();
             if row.is_empty() {
+                if row_number.eq(&(page.len() - 1)) {
+                    r.append_void(page_number + 1);
+                }
                 continue;
             }
             if !(row.contains("康方") || row.to_uppercase().starts_with("AKESO")) {
@@ -42,6 +45,10 @@ mod test_probe {
         let p = Path::new(
             r"D:\Studies\ak112\303\stats\CSR\product\output\bk\f-14-02-01-04-inve-pfs-for-fas.pdf",
         );
+        let report = probe(p).unwrap();
+        println!("{:?}", report);
+        let p =
+            Path::new(r"D:\Studies\ak105\302\stats\adhoc\product\output\l-16-02-04-03-mh-fas.pdf");
         let report = probe(p).unwrap();
         println!("{:?}", report);
     }
